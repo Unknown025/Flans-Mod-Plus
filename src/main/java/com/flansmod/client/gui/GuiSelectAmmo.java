46297d8c27a9d61285a13c5761e0c77139515e18
Андрей <51740316+SecretAgent12@users.noreply.github.com>
2022-01-09 17:53:38 +0200

    reworked GuiSelectAmmo#initGui()

--- src/main/java/com/flansmod/client/gui/GuiSelectAmmo.java
+++ src/main/java/com/flansmod/client/gui/GuiSelectAmmo.java
@@ -33,44 +33,35 @@ public class GuiSelectAmmo extends GuiScreen {
     protected int guiLeft;
     protected int guiTop;
     protected int guiTop2;
-    ArrayList<ItemStack> ammoList = new ArrayList<>();
-    ArrayList<ItemStack> ammoListForRender = new ArrayList<>();
-    ArrayList<ItemStack> inv = new ArrayList<>();
+    ArrayList<ShootableType> ammoForThisGun = new ArrayList<>();
+
     @Override
     public void initGui() {
-        ammoList.clear();
-        ammoListForRender.clear();
         guiLeft = (width - xSize) / 2;
         guiTop = (height - ySize) / 2;
         guiTop2 = guiTop - 30;
         buttonList.clear();
-
-        inv.clear();
-        for (ItemStack itemStack : mc.thePlayer.inventory.mainInventory) {
-            inv.add(itemStack);
-            if (mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemGun) {
-                ItemGun itemGun = (ItemGun) mc.thePlayer.getHeldItem().getItem();
-                if (itemStack != null) {
-                    Item item = itemStack.getItem();
-                    for (ShootableType ammoForCurrentGun : itemGun.type.ammo) {
-                        if (ammoForCurrentGun.getItem() != null) {
-                            if (ammoForCurrentGun.getItem() == item) {
-                                ammoList.add(itemStack);
-                            }
-                        }
-                    }
+        if (mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemGun) {
+            ItemGun itemGun = (ItemGun) mc.thePlayer.getHeldItem().getItem();
+            for (ShootableType shootableType : itemGun.type.ammo) {
+                ammoForThisGun.add(shootableType);
+            }
+            for (int i = 0; i < ammoForThisGun.size(); i++) {
+                int x = i > 5 ? 70 : 20;
+                int y = i > 5 ? i - 6 : i;
+                System.out.println(x);
+                buttonList.add(new GuiButton(i + 1, (guiLeft + x), guiTop2 + 10 + (y + 1) * 30, 20, 20, String.valueOf(i + 1)));
+            }
+        }
+        if (mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemGun) {
+            ItemGun itemGun = (ItemGun) mc.thePlayer.getHeldItem().getItem();
+            for (ShootableType shootableType : ammoForThisGun) {
+                if (shootableType.shortName.equals(itemGun.getPreferredAmmoStack(mc.thePlayer.getHeldItem()))) {
+                    GuiButton button = (GuiButton) buttonList.get(ammoForThisGun.indexOf(shootableType));
+                    button.enabled=false;
                 }
             }
         }
-        for (int i = 0; i < ammoList.size(); i++) {
-            int x = i>5?70:20;
-            int y = i>5?i-6:i;
-            System.out.println(x);
-            buttonList.add(new GuiButton(i + 1, (guiLeft + x), guiTop2 + 10 + (y + 1) * 30, 20, 20, String.valueOf(i + 1)));
-        }
-        ammoListForRender.clear();
-        ammoListForRender = (ArrayList<ItemStack>) ammoList.clone();
-        ammoList.clear();
         Keyboard.enableRepeatEvents(true);
     }
 
@@ -87,8 +78,12 @@ public class GuiSelectAmmo extends GuiScreen {
     @Override
     public void actionPerformed(GuiButton btn) {
         if (btn.enabled) {
-            ItemStack itemStack = ammoListForRender.get(btn.id-1);
-            FlansMod.getPacketHandler().sendToServer(new PacketSetPreferredAmmo(inv.indexOf(itemStack)));
+            FlansMod.getPacketHandler().sendToServer(new PacketSetPreferredAmmo(ammoForThisGun.get(btn.id - 1).shortName));
+            for(Object obj : buttonList){
+                GuiButton button = (GuiButton) obj;
+                button.enabled=true;
+            }
+            btn.enabled=false;
         }
     }
 
@@ -106,10 +101,10 @@ public class GuiSelectAmmo extends GuiScreen {
         RenderHelper.enableGUIStandardItemLighting();
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        for (int i = 0; i < ammoListForRender.size(); i++) {
-            int x = i>5?95:45;
-            int y = i>5?i-6:i;
-            drawSlotInventory(Minecraft.getMinecraft().fontRenderer, ammoListForRender.get(i), (guiLeft + x), guiTop2 + 10 + (y + 1) * 30);
+        for (int i = 0; i < ammoForThisGun.size(); i++) {
+            int x = i > 5 ? 95 : 45;
+            int y = i > 5 ? i - 6 : i;
+            drawSlotInventory(Minecraft.getMinecraft().fontRenderer, new ItemStack(ammoForThisGun.get(i).item), (guiLeft + x), guiTop2 + 10 + (y + 1) * 30);
         }
         GL11.glDisable(3042);
         RenderHelper.disableStandardItemLighting();
@@ -124,4 +119,3 @@ public class GuiSelectAmmo extends GuiScreen {
         itemRenderer.renderItemOverlayIntoGUI(fontRenderer, FlansModClient.minecraft.renderEngine, itemstack, i, j);
     }
 }
-
