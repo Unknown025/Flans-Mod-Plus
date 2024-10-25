b8aec3bb980115d9c37da8cbe9f357fec8c869f3
xdeadstokex <129391255+xdeadstokex@users.noreply.github.com>
2024-10-25 23:35:34 +0700

    Update ItemGun.java
    this and 2 last commit is an attempt to add an option to change mingun barrel spin speed by adding a new gun config option: MinigunAnimateSpeed float, with default is 2

--- src/main/java/com/flansmod/common/guns/ItemGun.java
+++ src/main/java/com/flansmod/common/guns/ItemGun.java
@@ -664,6 +664,7 @@ public class ItemGun extends Item implements IPaintableItem, IGunboxDescriptiona
                 
 //				animations.onGunEmpty(onLastBullet);
                 animations.doShoot(pumpDelay, pumpTime, hammerDelay, hammerAngle, althammerAngle, casingDelay);
+		animations.doshootminigun(gunType.minigunAnimateSpeed);
                 if (type.useFancyRecoil) {
                     FlansModClient.playerRecoil.addRecoil(type.getRecoil(stack));
                 }
