bb95f962364a9d1893ca1f4a4a725401f3058a20
xdeadstokex <129391255+xdeadstokex@users.noreply.github.com>
2024-10-27 10:04:16 +0700

    render custom barrel spin direction of mingun type RenderGun.java

--- src/main/java/com/flansmod/client/model/RenderGun.java
+++ src/main/java/com/flansmod/client/model/RenderGun.java
@@ -670,7 +670,7 @@ public class RenderGun implements IItemRenderer {
 		{
 			GL11.glPushMatrix();
 			GL11.glTranslatef(model.minigunBarrelOrigin.x, model.minigunBarrelOrigin.y, model.minigunBarrelOrigin.z);
-			GL11.glRotatef(animations.minigunBarrelRotation, 1F, 0F, 0F);
+			GL11.glRotatef(animations.minigunBarrelRotation, type.spinX, type.spinY, type.spinZ);
 			GL11.glTranslatef(-model.minigunBarrelOrigin.x, -model.minigunBarrelOrigin.y, -model.minigunBarrelOrigin.z);
 			model.renderMinigunBarrel(f);
 			GL11.glPopMatrix();
